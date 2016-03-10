ee065ca489835c819928b08d8bbf202f4c4fa399
13081111 <794146180@qq.com>
2016-03-10 15:06:54 +0800

    Finish Homework

--- 2/HOMEWORK-master/canvas_oop/canvas_oop.ts
+++ 2/HOMEWORK-master/canvas_oop/canvas_oop.ts
@@ -59,10 +59,12 @@ class Rect extends DisplayObject {
 }
 
 class TextField extends DisplayObject {
+    
+    color = '#FFFFFF';
 
     render(context: CanvasRenderingContext2D) {
         context.font = "20px Arial";
-        context.fillStyle = '#FFFFFF';
+        context.fillStyle = this.color;
         context.fillText('Homework2_13081111', 0, 20);
     }
 }
@@ -102,12 +104,7 @@ function loadResource(imageList, callback) {
 var canvas: HTMLCanvasElement = document.getElementById("game") as HTMLCanvasElement;
 var context = canvas.getContext("2d");
 
-
-var rect = new Rect();
-rect.width = 200;
-rect.height = 100;
-rect.color = '#00FF00'
-
+var X = 'Resource/';
 
 var rect2 = new Rect();
 rect2.width = 300;
@@ -118,25 +115,52 @@ rect2.rotation = Math.PI / 8;
 rect2.color = '#00FFFF'
 
 var text = new TextField();
-text.x = 155;
-text.y = 312;
+text.x = 116;
+text.y = 300;
+text.color = '#DDDDDD'
 
 var bitmap = new Bitmap();
-bitmap.source = 'Resource/DS_BG.jpg';
+bitmap.source =X+'DS_BG.jpg';
 bitmap.x = 0;
 bitmap.y = 0;
 
 var RB1 = new Bitmap();
-RB1.source = 'Resource/RB1.jpg';
+RB1.source =  X+'RB1.jpg';
 RB1.x = 1083;
 RB1.y = 286;
+
+var RB2 = new Bitmap();
+RB2.source = X+'RB2.jpg';
+RB2.x = 1086;
+RB2.y = 362;
+
+var RB3 = new Bitmap();
+RB3.source = X+'RB3.jpg';
+RB3.x = 1086;
+RB3.y = 422;
+
+var RB4 = new Bitmap();
+RB4.source = X+'RB4.jpg';
+RB4.x = 1083;
+RB4.y = 481;
+
+var RB5 = new Bitmap();
+RB5.source = X+'RB5.jpg';
+RB5.x = 1101;
+RB5.y = 636;
+
+var RB6 = new Bitmap();
+RB6.source = X+'RB6.jpg';
+RB6.x = 1101;
+RB6.y = 682;
 
 
 
 //渲染队列
-var renderQueue = [text,bitmap,RB1];
+var renderQueue =[bitmap,text,RB1,RB2,RB3,RB4,RB5,RB6];
 //资源加载列表
-var imageList = ['Resource/DS_BG.jpg','Resource/RB1.jpg'];
+var imageList = ['Resource/DS_BG.jpg','Resource/RB1.jpg',
+                 'Resource/RB2.jpg','Resource/RB3.jpg','Resource/RB4.jpg','Resource/RB5.jpg','Resource/RB6.jpg'];
 
 //先加载资源，加载成功之后执行渲染队列
 loadResource(imageList, function() {
