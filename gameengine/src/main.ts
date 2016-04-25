aa635eb69e911bb63b3f83c2c91b73860ac563a9
13081111 <794146180@qq.com>
2016-04-25 18:27:59 +0800

    Homework 8 Change

--- gameengine/src/main.ts
+++ gameengine/src/main.ts
@@ -1,5 +1,31 @@
 
 
+function readFile() {
+    var map_path = __dirname + "/map.json"
+    var content = fs.readFileSync(map_path, "utf-8");
+    var obj = JSON.parse(content);
+    var mapData = obj.map;
+    return mapData;
+}
+
+function changMap(rows,clows,changecolor){
+    var mapDate = readFile;
+    mapData[clows][rows] = changecolor;  
+    return mapData
+}
+
+function saveMap() {
+    var map_path = __dirname + "/map.json";
+    var content = fs.readFileSync(map_path,"utf-8");
+    var obj = JSON.parse(content);
+    obj.map = mapData;
+    var objS = JSON.stringify(obj);
+    fs.writeFileSync(map_path,objS,"utf-8");
+    console.log("savemap");
+}
+
+
+
 function createMapEditor() {
     var world = new editor.WorldMap();
     var rows = mapData.length;
@@ -21,6 +47,18 @@ function createMapEditor() {
             eventCore.register(tile, events.displayObjectRectHitTest, onTileClick);
         }
     }
+    
+     // 网格以外的图元创建
+    var Btn_save = new render.Bitmap();
+    Btn_save.source = "Resource/Btn_Save.png";
+    Btn_save.x = 0;
+    Btn_save.y = 220;
+    renderCore.start(Btn_save,["Resource/Btn_Save.png"]);
+    
+    eventCore.register(Btn_save, events.displayObjectRectHitTest, onBtnSaveClick);
+     
+    world.addChild(Btn_save);
+    
     return world;
 
 }
@@ -29,7 +67,32 @@ function createMapEditor() {
 
 function onTileClick(tile: editor.Tile) {
     console.log(tile);
+    //0 == red 1==blue
+    
+    var changeXnum = 0, changeYnum = 0;
+    changeXnum = tile.x / 50;
+    changeYnum = tile.y / 50;
+    
+    //检测之前的颜色
+    if (mapData[changeYnum][changeXnum] == 1) {
+        tile.color = "#FF0000";
+        var targetColor = 0;
+    }
+    else {
+        tile.color = "#0000FF";
+        var targetColor = 1;
+    }
+
+   
+    //change map
+    changMap(changeXnum, changeYnum, targetColor);
 }
+
+function onBtnSaveClick() {
+    saveMap();
+    console.log("Hit");
+}
+
 
 var storage = data.Storage.getInstance();
 storage.readFile();
